7b4cf0c8886fc0c5f0d5d0020390ea0e29bfbcdb
Ranger <yemzero0@gmail.com>
2024-11-02 12:46:42 +0530

    Changed some indication logic 🧠

--- src/microcontroller/main/main.ino
+++ src/microcontroller/main/main.ino
@@ -7,9 +7,11 @@ const char* password = "Useless@24";
 
 const char* serverHost = "ntfy.sh";
 
+dht11 DHT11;
+
+int builtinLed = 2;
 int sensorPin = 4;  // D2
 
-dht11 DHT11;
 int timeout = 10;
 
 int lastTemp;
@@ -17,15 +19,20 @@ int lastTemp;
 void  setup()
 {
   Serial.begin(9600);
+
+  pinMode(builtinLed, OUTPUT);
   
   WiFi.mode(WIFI_STA);
   WiFi.begin(SSID, password);
   Serial.println("Started!");
 
   while (WiFi.status() != WL_CONNECTED && timeout > 0) {
+    digitalWrite(builtinLed, HIGH);
     Serial.println("Connecting....");
     timeout--;
-    delay(500);
+    delay(250);
+    digitalWrite(builtinLed, LOW);
+    delay(150);
   }
 
   if (WiFi.status() == WL_CONNECTED) {
@@ -33,6 +40,19 @@ void  setup()
     Serial.print(SSID);
     Serial.print("# IP address: ");
     Serial.println(WiFi.localIP());
+
+    digitalWrite(builtinLed,HIGH);
+    delay(200);
+    digitalWrite(builtinLed, LOW);
+    delay(100);
+    digitalWrite(builtinLed,HIGH);
+    delay(200);
+    digitalWrite(builtinLed, LOW);
+    delay(100);
+    digitalWrite(builtinLed,HIGH);
+    delay(200);
+    digitalWrite(builtinLed, LOW);
+
   } else {
     Serial.print("Connection failed!");
   }
@@ -43,14 +63,20 @@ void  setup()
 void sendTempData(int temp) {
   WiFiClient client;
 
+  digitalWrite(builtinLed,HIGH);
+
   Serial.print("sending...");
+  delay(100);
 
   if (!client.connect(serverHost, 80)) {
+    digitalWrite(builtinLed, LOW);
     Serial.println("Connection to server failed!");
     return;
   }
   String url = "/thaapasakhi-server";
   String payload = String(temp);
+
+  digitalWrite(builtinLed, LOW);
 
   client.print("POST " + url + " HTTP/1.1\r\n" +
                "Host: " + serverHost + "\r\n" +
@@ -58,7 +84,6 @@ void sendTempData(int temp) {
                "Content-Length: " + payload.length() + "\r\n" +
                "Connection: close\r\n\r\n" +
                payload);
-
 }
 
 void loop()
